b2839ec0d192698bf9e757086d75996fa230a72f
barelysoftware <mathias@barelysoftware.com>
2014-09-16 23:08:49 +0200

    Added Nancy module

---
+++ Mvc/Modules/ColorMeBadModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace Mvc.Modules
+{
+    public class ColorMeBadModule : NancyModule
+    {
+        public ColorMeBadModule() : base("/nancy")
+        {
+            Get["/colormebad/{color}"] = parameters =>
+                {
+                    return string.Concat("<div class=\"test-block ", parameters.color,
+                    " clearfix\">&nbsp;</div>");
+                };
+        }
+    }
+}
