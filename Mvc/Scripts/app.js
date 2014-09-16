66f78503413624c7156b8e1e63b6ed3e8799a2b5
barelysoftware <mathias@barelysoftware.com>
2014-09-16 22:06:36 +0200

    Added Javascript base for ApiController

---
+++ Mvc/Scripts/app.js
@@ -0,0 +1,17 @@
+﻿//(function () {
+$(document).ready(function() {
+    $(".tile > a").click('click', function(evt) {
+        evt.preventDefault();
+        var $target = $(evt.target);
+        var link = $target.closest("a");
+        var color = link.data('color');
+
+        console.log(color);
+
+        $.getJSON("/api/colorme/", { 'color': color },function(data) {
+            $("#content").html(data);
+        });
+    });
+});
+
+//})(jQuery);
