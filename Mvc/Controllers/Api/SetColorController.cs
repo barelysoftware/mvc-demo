91736c56ac16bb420fb31e1ff488f55006462a48
barelysoftware <mathias@barelysoftware.com>
2014-09-16 22:24:29 +0200

    Added WebAPI controller

---
+++ Mvc/Controllers/Api/SetColorController.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Web.Http;
+
+namespace Mvc.Controllers.Api
+{
+    public class ColorMeController : ApiController
+    {
+        public string Get([FromUri]string color)
+        {
+            return string.Concat("<div class=\"test-block ", color," clearfix\">&nbsp;</div>");
+        }
+    }
+}
