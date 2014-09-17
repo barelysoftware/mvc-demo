e51fe2ab97fc9208b42698a4b967e03de42238ee
barelysoftware <mathias@barelysoftware.com>
2014-09-17 11:22:27 +0200

    Fixed Nancy module that it works

--- Mvc/App_Start/RouteConfig.cs
+++ Mvc/App_Start/RouteConfig.cs
@@ -1,23 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace Mvc
+namespace Mvc.App_Start
 {
     public class RouteConfig
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.IgnoreRoute("nancy/{*pathinfo}");
+        
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
         }
     }
 }
---
+++ Mvc/Nancy/ColorMeBadModule.cs
@@ -0,0 +1,19 @@
+﻿using Nancy;
+
+namespace Mvc.Modules
+{
+    public class ColorMeBadModule : NancyModule
+    {
+        public ColorMeBadModule()
+            : base("/nancy")
+        {
+            Get["/colormebad"] = parameters => "Yo!";
+
+            Get["/colormebad/{color}"] = parameters =>
+                                         !string.IsNullOrEmpty(parameters["color"])
+                                             ? string.Concat("<div class=\"test-block ", parameters["color"],
+                                                             " clearfix\">&nbsp;</div>")
+                                             : "";
+        }
+    }
+}
